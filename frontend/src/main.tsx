1ff1e83f2cc45c6ceb5f82b10f5b59ea10efbe7c
williamjesusdev <williamjesusti@gmail.com>
2022-09-09 18:22:54 -0300

    chore: adjust for deploy

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,8 @@ import App from "./App";
 
 import "./index.css";
 
+window.React = React;
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
