9c25765e89fc298894537ca82bd798e1933cb736
williamjesusdev <williamjesusti@gmail.com>
2022-09-09 17:38:41 -0300

    feature: send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,15 +1,34 @@
-import nofification from "../../assets/img/notification-icon.svg";
+import { Dispatch, SetStateAction, useState } from "react";
+import axios from "axios";
+
+import { BASE_URL } from "../../utils/request";
+import NotificationIcon from "../NotificationIcon";
 
 import "./styles.css";
 
 type Props = {
-  saleId: Number;
+  saleId: number;
 };
 
+type SendNotificationProps = {
+  saleId: number;
+  setSent: Dispatch<SetStateAction<string>>;
+};
+
+function sendNotification({ saleId, setSent }: SendNotificationProps) {
+  axios
+    .get(`${BASE_URL}/sales/${saleId}/notification`)
+    .then(() => setSent("sent"));
+}
+
 function NotificationButton({ saleId }: Props) {
+  const [sent, setSent] = useState("");
   return (
-    <div className="dsmeta-red-btn">
-      <img src={nofification} alt="Notificar" />
+    <div
+      className={`dsmeta-red-btn ${sent}`}
+      onClick={() => sendNotification({ saleId, setSent })}
+    >
+      <NotificationIcon />
     </div>
   );
 }
