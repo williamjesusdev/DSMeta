ca4e6643c411674d59e669b15f9fa2ed7ed714ed
williamjesusdev <williamjesusti@gmail.com>
2022-09-09 15:01:45 -0300

    fix: adjust component structure

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,12 +2,14 @@ import nofification from "../../assets/img/notification-icon.svg";
 
 import "./styles.css";
 
-function NotificationButton() {
+type Props = {
+  saleId: Number;
+};
+
+function NotificationButton({ saleId }: Props) {
   return (
-    <div className="dsmeta-red-btn-container">
-      <div className="dsmeta-red-btn">
-        <img src={nofification} alt="Notificar" />
-      </div>
+    <div className="dsmeta-red-btn">
+      <img src={nofification} alt="Notificar" />
     </div>
   );
 }
